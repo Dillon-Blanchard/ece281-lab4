d6087e95d29fa1326bc9d4369e16ea12953323ba
Dillon-Blanchard <c26dillon.blanchard@afacademy.af.edu@users.noreply.github.com>
2024-04-15 20:50:17 -0600

    Final Submission

--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -92,21 +92,92 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
   
 	-- declare components and signals
+component sevenSegDecoder is
+                port (
+                i_D : in std_logic_vector (3 downto 0);
+                o_S : out std_logic_vector (6 downto 0)     
+    
+    );
+          end component sevenSegDecoder;
+
+component clock_divider is
+              generic ( constant k_DIV : natural := 2    ); -- How many clk cycles until slow clock toggles
+                                                         -- Effectively, you divide the clk double this 
+                                                         -- number (e.g., k_DIV := 2 --> clock divider of 4)
+              port (     i_clk    : in std_logic;
+                      i_reset  : in std_logic;           -- asynchronous
+                      o_clk    : out std_logic           -- divided (slow) clock
+              );
+          end component clock_divider;
+component elevator_controller_fsm is
+              Port ( i_clk     : in  STD_LOGIC;
+                     i_reset   : in  STD_LOGIC;
+                     i_stop    : in  STD_LOGIC;
+                     i_up_down : in  STD_LOGIC;
+                     o_floor   : out STD_LOGIC_VECTOR (3 downto 0)           
+                   );
+          end component elevator_controller_fsm;
+         
+type sm_floor is (s_floor1, s_floor2, s_floor3, s_floor4);
+          
+          -- Here you create variables that can take on the values defined above. Neat!    
+signal f_Q, f_Q_next: sm_floor;
+signal w_7SD_EN_n : std_logic;
+          
+signal w_clk : std_logic;
+signal w_floor : std_logic_vector(3 downto 0) := (others => '0');
+
+-- 50 MHz clock
+constant k_clk_period : time := 20 ns;
 
   
 begin
 	-- PORT MAPS ----------------------------------------
+        clkdiv_inst : clock_divider         --instantiation of clock_divider to take 
+        generic map ( k_DIV => 50000000 ) -- 1 Hz clock from 100 MHz
+        port map (                          
+            i_clk   => clk,
+            i_reset => btnL or btnU,
+            o_clk   => w_clk
+        ); 
+    sevenSegDecoder_inst : sevenSegDecoder
+        port map(
+            i_D(0) => sw(0),
+            i_D(1) => sw(1),
+            i_D(2) => sw(2),
+            i_D(3) => sw(3),
+            o_S(0) => seg(0),
+            o_S(1) => seg(1),
+            o_S(2) => seg(2),
+            o_S(3) => seg(3),
+            o_S(4) => seg(4),
+            o_S(5) => seg(5),
+            o_S(6) => seg(6)
+        ); 
+	uut_inst : elevator_controller_fsm port map (
+            i_clk     => w_clk,
+            i_reset   => btnR or btnL,
+            i_stop    => sw(0),
+            i_up_down => sw(1),
+            o_floor   => w_floor
+        );
+        
 
+        
 	
 	
 	-- CONCURRENT STATEMENTS ----------------------------
-	
+
 	-- LED 15 gets the FSM slow clock signal. The rest are grounded.
-	
+	led(15) <= w_clk;
+	led(14 downto 0) <= (others => '0');
 
 	-- leave unused switches UNCONNECTED. Ignore any warnings this causes.
 	
 	-- wire up active-low 7SD anodes (an) as required
 	-- Tie any unused anodes to power ('1') to keep them off
-	
+	an(1)  <= w_7SD_EN_n;
+	an(0) <= '1';
+	an(2) <= '1';
+	an(3) <= '1';
 end top_basys3_arch;
